4ca2fb6cd5916d763266e905500c5b0fe258ed91
Fabio Matsunaga <fabio.matsunaga@sesisenaipr.org.br>
2021-11-05 17:35:33 -0300

    style correction

--- src/TodoList.js
+++ src/TodoList.js
@@ -43,57 +43,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
-  complete: {
-    textDecorationLine: 'line-through',
-    fontSize: 18,
-    flex: 1,
-    flexWrap: 'wrap'
-  },
-  incomplete: {
-    fontSize: 18,
-    flex: 1,
-    flexWrap: 'wrap'
-  },
-  toggle: {
-    flexDirection: 'row',
-  },
-  item: {
-    flex: 1,
-    flexDirection: 'row',
-    marginBottom: 5,
-    alignItems: 'center',
-    paddingRight: 10,
-    borderColor: '#d0dde2',
-    borderWidth: 0.5,
-    borderRadius: 12,
-    backgroundColor: '#fff',
-    paddingVertical: 3,
-  },
-  rightSwipe: {
-    flex: 1,
-    backgroundColor: '#af5b5e',
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    paddingRight: 15,
-    marginBottom: 5,
-  },
-  leftSwipeComplete: {
-    flex: 1,
-    backgroundColor: '#3293b3',
-    borderRadius: 12,
-    justifyContent: 'center',
-    paddingLeft: 15,
-    marginBottom: 5,
-  },
-  leftSwipeIncomplete: {
-    flex: 1,
-    backgroundColor: '#20b286',
-    borderRadius: 12,
-    justifyContent: 'center',
-    paddingLeft: 15,
-    marginBottom: 5,
-  },
 });
 
 export default TodoList;
